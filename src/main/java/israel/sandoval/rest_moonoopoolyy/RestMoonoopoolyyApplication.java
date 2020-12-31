4587f224e9080f1959cf434b536c38877601b1a2
israel.sandoval <israel.sandoval.ps@icloud.com>
2020-12-30 18:37:40 -0600

    Commit inicial: proyecto en blanco

---
+++ src/main/java/israel/sandoval/rest_moonoopoolyy/RestMoonoopoolyyApplication.java
@@ -0,0 +1,13 @@
+package israel.sandoval.rest_moonoopoolyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMoonoopoolyyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMoonoopoolyyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/israel/sandoval/rest_moonoopoolyy/RestMoonoopoolyyApplicationTests.java
@@ -0,0 +1,13 @@
+package israel.sandoval.rest_moonoopoolyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMoonoopoolyyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
